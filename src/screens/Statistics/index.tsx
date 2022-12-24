962ab72c880f3e9cee870d006d3174e60d2f8307
MarcosVel <marcos.veloso@sempreceub.com>
2022-12-24 17:24:38 -0300

    feat: style of statistics screen

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -1,10 +1,65 @@
-import { View, Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
+import { useTheme } from "styled-components/native";
+import { Label } from "../../components/Ui/styles";
+import { Card, Container, Diet, GoBack, Stats } from "./styles";
 
 export default function Statistics() {
+  const { FONT_SIZE, COLORS } = useTheme();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Statistics</Text>
-    </View>
+    <Container>
+      <GoBack onPress={() => navigation.goBack()}>
+        <Feather name="arrow-left" size={24} color={COLORS.green_dark} />
+      </GoBack>
+      <Label size={FONT_SIZE.XG} color={COLORS.gray_100} mt={8} mb={2} bold>
+        90,86%
+      </Label>
+      <Label size={FONT_SIZE.SM}>das refeições dentro da dieta</Label>
+
+      <Stats>
+        <Label size={FONT_SIZE.SM} color={COLORS.gray_100} mb={24} bold>
+          Estatísticas gerais
+        </Label>
+
+        <Card>
+          <Label size={FONT_SIZE.XL} color={COLORS.gray_100} mb={8} bold>
+            22
+          </Label>
+          <Label size={FONT_SIZE.SM}>
+            melhor sequência de pratos dentro da dieta
+          </Label>
+        </Card>
+
+        <Card>
+          <Label size={FONT_SIZE.XL} color={COLORS.gray_100} mb={8} bold>
+            109
+          </Label>
+          <Label size={FONT_SIZE.SM}>refeições registradas</Label>
+        </Card>
+
+        <Diet>
+          <Card color={COLORS.green_light} style={{ marginRight: 12 }}>
+            <Label size={FONT_SIZE.XL} color={COLORS.gray_100} mb={8} bold>
+              99
+            </Label>
+            <Label size={FONT_SIZE.SM} style={{ textAlign: "center" }}>
+              refeições dentro da dieta
+            </Label>
+          </Card>
+
+          <Card color={COLORS.red_light}>
+            <Label size={FONT_SIZE.XL} color={COLORS.gray_100} mb={8} bold>
+              99
+            </Label>
+            <Label size={FONT_SIZE.SM} style={{ textAlign: "center" }}>
+              refeições dentro da dieta
+            </Label>
+          </Card>
+        </Diet>
+      </Stats>
+    </Container>
   );
 }
