adcb6d935a22ff119d6ab540c0b3a8e5409b5b0c
MarcosVel <marcos.veloso@sempreceub.com>
2022-12-27 04:19:21 -0300

    feat: creating new meal screen

---
+++ src/screens/NewMeal/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import Header from "../../components/Header";
+import Input from "../../components/Input";
+import { RoundedContainer } from "../../components/Ui/styles";
+import { Container, Date } from "./styles";
+
+export default function NewMeal() {
+  return (
+    <Container>
+      <Header title="Nova refeição" />
+
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <RoundedContainer>
+          <Input label="Nome" />
+
+          <Input label="Descrição" multiline />
+
+          <Date>
+            <Input label="Data" mask="99/99/9999" style={{ marginRight: 20 }} />
+            <Input label="Hora" mask="99:99" />
+          </Date>
+        </RoundedContainer>
+      </TouchableWithoutFeedback>
+    </Container>
+  );
+}
---
+++ src/screens/NewMeal/styles.ts
@@ -0,0 +1,11 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import styled from "styled-components/native";
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.gray_500};
+`;
+
+export const Date = styled.View`
+  flex-direction: row;
+`;
